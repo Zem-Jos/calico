dc92889b8d5a13dccae5a4cc25ec816a1f5322f8
josiahfs <josiahfrlswt@gmail.com>
2023-02-22 02:20:42 +0700

    create homepage ui

---
+++ lib/theme.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color lightBrownColor = Color(0xffE1A071);
+const Color brownColor = Color(0xffC87C47);
+const Color darkBrownColor = Color(0xff433230);
+const Color darkGrayColor = Color(0xff2F2F2F);
+const Color blackColor = Color(0xff2F2F30);
+const Color whiteColor = Color(0xffffffff);
+
+double edge = 24.0;
+
+// TextStyle blackTextStyle = GoogleFonts.rubik(
+//   color: blackColor,
+//   fontWeight: FontWeight.w500,
+// );
+// TextStyle greyTextStyle = GoogleFonts.rubik(
+//   color: greyColor,
+//   fontWeight: FontWeight.w300,
+// );
+// TextStyle whiteTextStyle = GoogleFonts.rubik(
+//   color: whiteColor,
+//   fontWeight: FontWeight.w500,
+// );
+// TextStyle purpleTextStyle = GoogleFonts.rubik(
+//   color: purpleColor,
+//   fontWeight: FontWeight.w500,
+// );
+// TextStyle regularTextStyle = GoogleFonts.rubik(
+//   color: blackColor,
+//   fontWeight: FontWeight.w400,
+// );
