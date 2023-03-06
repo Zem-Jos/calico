129a7b7c25bcb62c6c6ba018e7e0ab55a2d28e01
josiahfs <josiahfrlswt@gmail.com>
2023-03-06 16:55:12 +0700

    change app icon and splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,64 @@ import 'package:calico/views/goto_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'controllers/authentication_controller.dart';
+
+// ThemeData _darkTheme = ThemeData(
+//     brightness: Brightness.dark,
+//     primaryColor: Colors.amber,
+//     colorScheme: ColorScheme.dark(),
+//     buttonTheme: ButtonThemeData(
+//       buttonColor: Colors.amber,
+//       disabledColor: Colors.grey,
+//     ));
+
+// ThemeData _lightTheme = ThemeData(
+//     brightness: Brightness.light,
+//     primaryColor: Colors.blue,
+//     colorScheme: ColorScheme.dark(),
+//     buttonTheme: ButtonThemeData(
+//       buttonColor: Colors.blue,
+//       disabledColor: Colors.grey,
+//     ));
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp().then((value) => Get.put(AuthController()));
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  // RxBool _isLightTheme = false.obs;
+
+  // Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  // _saveThemeStatus() async {
+  //   SharedPreferences pref = await _prefs;
+  //   pref.setBool('theme', _isLightTheme.value);
+  // }
+
+  // _getThemeStatus() async {
+  //   var _isLight = _prefs.then((SharedPreferences prefs) {
+  //     return prefs.getBool('theme') != null ? prefs.getBool('theme') : true;
+  //   }).obs;
+  //   _isLightTheme.value = (await _isLight.value)!;
+  //   Get.changeThemeMode(_isLightTheme.value ? ThemeMode.light : ThemeMode.dark);
+  // }
+
+  // MainApp() {
+  //   _getThemeStatus();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
+      // theme: _lightTheme,
+      // darkTheme: _darkTheme,
+      themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
+      home: const Scaffold(
         body: GotoScreen(),
       ),
     );
