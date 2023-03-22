e760a0c459c7cc595aec55993094e7e345f77e56
josiahfs <josiahfrlswt@gmail.com>
2023-03-22 22:47:23 +0700

    add breathing guide progress bar

--- lib/controllers/breathing_controller.dart
+++ lib/controllers/breathing_controller.dart
@@ -1,50 +1,64 @@
 import 'dart:async';
-
-import 'package:flutter/material.dart';
+import 'package:calico/widgets/navigation_bar.dart';
 import 'package:get/get.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
 
 class BreathingController extends GetxController {
   RxBool isExpanded = false.obs;
   RxBool isHold = false.obs;
   RxBool isStarted = false.obs;
   RxInt iteration = 0.obs;
-  Rx<Duration> currentTime = Duration(seconds: 0).obs;
-  Rx<Duration> totalDuration = Duration(seconds: 152).obs;
 
-  void onTap() async {
-    if (iteration.value < 8) {
+  void onTap() {
+    if (iteration.value < 7) {
       isStarted.value = true;
       isExpanded.value = true;
       // After 4 seconds, set the _isExpanded flag to false to trigger the hold animation
-      Future.delayed(Duration(seconds: 4), () {
+      Future.delayed(const Duration(seconds: 4), () {
         isHold.value = true;
       });
       // After 7 seconds, set the _isHold flag to false to trigger the shrink animation
-      await Future.delayed(Duration(seconds: 7), () {
+      Future.delayed(const Duration(seconds: 11), () {
         isHold.value = false;
         isExpanded.value = false;
       });
       // After 8 seconds, set the _isExpanded flag to true to trigger the hold animation
-      await Future.delayed(Duration(seconds: 8), () {
+      Future.delayed(const Duration(seconds: 19), () {
         onTap();
         iteration.value++;
       });
     }
   }
 
-  // var progress = 0.0.obs;
-  // Timer? _timer;
+  Timer? timer;
+  final count = 0.obs;
+  final progress = 0.0.obs;
 
-  // void startTimer() {
-  //   _timer?.cancel();
-  //   _timer = Timer.periodic(const Duration(seconds: 10), (timer) {
-  //     progress.value = timer.tick / (2 * 60 + 32) * 100;
-  //     if (timer.tick >= 2 * 60 + 32 * 1000 / 10) {
-  //       timer.cancel();
-  //     }
-  //   });
-  // }
+  void startTimer() {
+    timer?.cancel();
+    timer = Timer.periodic(const Duration(seconds: 1), (Timer t) {
+      if (count.value >= 152) {
+        t.cancel();
+        Get.offAll(const NavigationPage());
+      } else {
+        count.value += 1;
+        progress.value = count.value / 152;
+      }
+    });
+  }
+
+  int getMinutes() {
+    return (count.value / 60).floor();
+  }
+
+  int getSeconds() {
+    return count.value % 60;
+  }
+
+  void resetTimer() {
+    count.value = 0;
+    progress.value = 0.0;
+    timer?.cancel();
+  }
 
   @override
   void onClose() {
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -3,10 +3,8 @@ import 'package:calico/controllers/theme_controller.dart';
 import 'package:calico/theme.dart';
 import 'package:calico/views/breathing_screen.dart';
 import 'package:calico/views/chat_screen.dart';
-import 'package:calico/views/expert_screen.dart';
 import 'package:calico/views/grounding_screen.dart';
 import 'package:calico/views/sos_screen.dart';
-import 'package:calico/widgets/article_card.dart';
 import 'package:calico/widgets/mood_board.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -22,9 +20,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  // TestController testController = Get.put(TestController());
   AuthController authController = Get.find<AuthController>();
-  final ThemeController _themeController = Get.find<ThemeController>();
   final ColorController _colorController = Get.put(ColorController());
   final BottomNavigationController _bottomNavigationController =
       Get.put(BottomNavigationController());
@@ -43,13 +39,14 @@ class _HomeScreenState extends State<HomeScreen> {
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height * 0.8,
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.vertical(top: Radius.circular(60)),
+                borderRadius:
+                    const BorderRadius.vertical(top: Radius.circular(60)),
                 color: _colorController.getActivityBackgroundColor(),
               ),
             ),
           ),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: edge),
+            padding: const EdgeInsets.symmetric(horizontal: edge),
             child: Column(
               children: [
                 const SizedBox(
@@ -165,10 +162,23 @@ class _HomeScreenState extends State<HomeScreen> {
                   onTap: () {
                     _bottomNavigationController.changeIndex(2);
                   },
-                  child: Image.asset(
-                    'assets/images/expert_button.png',
-                    width: 342,
-                    height: 97,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0xff433230).withOpacity(0.15),
+                          spreadRadius: 0,
+                          blurRadius: 12,
+                          offset:
+                              const Offset(0, 4), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: Image.asset(
+                      'assets/images/expert_button.png',
+                      width: 342,
+                      height: 97,
+                    ),
                   ),
                 ),
                 const SizedBox(
@@ -178,10 +188,23 @@ class _HomeScreenState extends State<HomeScreen> {
                   onTap: () {
                     Get.to(BreathingScreen());
                   },
-                  child: Image.asset(
-                    'assets/images/breathing_button.png',
-                    width: 342,
-                    height: 97,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0xff433230).withOpacity(0.15),
+                          spreadRadius: 0,
+                          blurRadius: 12,
+                          offset:
+                              const Offset(0, 4), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: Image.asset(
+                      'assets/images/breathing_button.png',
+                      width: 342,
+                      height: 97,
+                    ),
                   ),
                 ),
                 const SizedBox(
@@ -191,10 +214,23 @@ class _HomeScreenState extends State<HomeScreen> {
                   onTap: () {
                     Get.to(const GroundingScreen());
                   },
-                  child: Image.asset(
-                    'assets/images/grounding_button.png',
-                    width: 342,
-                    height: 97,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0xff433230).withOpacity(0.15),
+                          spreadRadius: 0,
+                          blurRadius: 12,
+                          offset:
+                              const Offset(0, 4), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: Image.asset(
+                      'assets/images/grounding_button.png',
+                      width: 342,
+                      height: 97,
+                    ),
                   ),
                 ),
                 const SizedBox(
