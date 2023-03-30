a6dd465ca06cddf8a41440610d18f6a0c159c8cb
josiahfs <josiahfrlswt@gmail.com>
2023-03-30 16:19:48 +0700

    fix theme state bug

--- lib/views/settings_screen.dart
+++ lib/views/settings_screen.dart
@@ -19,169 +19,85 @@ class _SettingsScreenState extends State<SettingsScreen> {
   final ColorController _colorController = Get.put(ColorController());
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: _colorController.getBackgroundColor(),
-      appBar: AppBar(
-        elevation: 0,
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.transparent,
-        flexibleSpace: SafeArea(
-          child: Container(
-            height: 65,
-            color: Colors.transparent,
-            padding: const EdgeInsets.only(right: 16),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        Get.off(NavigationPage());
-                      },
-                      icon: const Icon(
-                        Icons.arrow_back,
-                        color: brownColor,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: _colorController.getContainerColor(),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
+    return WillPopScope(
+      onWillPop: () async {
+        Get.off(NavigationPage());
+        return true;
+      },
+      child: Scaffold(
+        backgroundColor: _colorController.getBackgroundColor(),
+        appBar: AppBar(
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.transparent,
+          flexibleSpace: SafeArea(
+            child: Container(
+              height: 65,
+              color: Colors.transparent,
+              padding: const EdgeInsets.only(right: 16),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(
                     children: [
-                      Image.asset(
-                        'assets/images/icon/avatar.png',
-                        width: 45,
-                      ),
-                      Text(
-                        authController.user?.displayName ?? 'Tamu',
-                        style: GoogleFonts.rubik(
-                            fontSize: 20, fontWeight: FontWeight.w400),
-                      ),
-                      SizedBox(height: 4),
-                      Text(
-                        authController.user?.email ?? 'anonim',
-                        style: GoogleFonts.rubik(
-                            fontSize: 12,
-                            fontWeight: FontWeight.w400,
-                            color: grayColor),
+                      IconButton(
+                        onPressed: () {
+                          Get.off(NavigationPage());
+                        },
+                        icon: const Icon(
+                          Icons.arrow_back,
+                          color: brownColor,
+                        ),
                       ),
                     ],
                   ),
-                ),
+                ],
               ),
-              SizedBox(height: 8),
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: _colorController.getContainerColor(),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 8.0, horizontal: 16.0),
-                  child: Row(children: [
-                    Image.asset(
-                      'assets/images/icon/notif_icon.png',
-                      width: 35,
+            ),
+          ),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: _colorController.getContainerColor(),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/icon/avatar.png',
+                          width: 45,
+                        ),
+                        Text(
+                          authController.user?.displayName ?? 'Tamu',
+                          style: GoogleFonts.rubik(
+                              fontSize: 20, fontWeight: FontWeight.w400),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          authController.user?.email ?? 'anonim',
+                          style: GoogleFonts.rubik(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                              color: grayColor),
+                        ),
+                      ],
                     ),
-                    SizedBox(width: 16),
-                    Text(
-                      'Notifikasi',
-                      style: GoogleFonts.rubik(
-                          fontSize: 17, fontWeight: FontWeight.w400),
-                    ),
-                  ]),
+                  ),
                 ),
-              ),
-              SizedBox(height: 8),
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: _colorController.getContainerColor(),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 2.0, horizontal: 16.0),
-                  child: Row(children: [
-                    Image.asset(
-                      'assets/images/icon/dark_icon.png',
-                      width: 35,
-                    ),
-                    const SizedBox(width: 16),
-                    Text(
-                      'Mode Gelap',
-                      style: GoogleFonts.rubik(
-                          fontSize: 17, fontWeight: FontWeight.w400),
-                    ),
-                    const Spacer(),
-                    Obx(() => Switch(
-                          activeColor: brownColor,
-                          activeTrackColor: lightBrownColor,
-                          inactiveThumbColor: brownColor,
-                          inactiveTrackColor: lightGrayColor,
-                          value: _themeController.isDarkMode.value,
-                          onChanged: (value) {
-                            setState(() {
-                              _themeController.changeTheme();
-                            });
-                          },
-                        )),
-                  ]),
-                ),
-              ),
-              const SizedBox(height: 8),
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: _colorController.getContainerColor(),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 8.0, horizontal: 16.0),
-                  child: Row(children: [
-                    Image.asset(
-                      'assets/images/icon/term_icon.png',
-                      width: 35,
-                    ),
-                    SizedBox(width: 16),
-                    Text(
-                      'Ketentuan Penggunaan',
-                      style: GoogleFonts.rubik(
-                          fontSize: 17, fontWeight: FontWeight.w400),
-                    ),
-                  ]),
-                ),
-              ),
-              SizedBox(height: 8),
-              GestureDetector(
-                onTap: () {
-                  AuthController.instance.logout();
-                },
-                child: Container(
+                SizedBox(height: 8),
+                Container(
                   width: double.infinity,
                   decoration: BoxDecoration(
                     color: _colorController.getContainerColor(),
@@ -192,20 +108,110 @@ class _SettingsScreenState extends State<SettingsScreen> {
                         vertical: 8.0, horizontal: 16.0),
                     child: Row(children: [
                       Image.asset(
-                        'assets/images/icon/out_icon.png',
+                        'assets/images/icon/notif_icon.png',
                         width: 35,
                       ),
                       SizedBox(width: 16),
                       Text(
-                        'Keluar',
+                        'Notifikasi',
                         style: GoogleFonts.rubik(
                             fontSize: 17, fontWeight: FontWeight.w400),
                       ),
                     ]),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 8),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: _colorController.getContainerColor(),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 2.0, horizontal: 16.0),
+                    child: Row(children: [
+                      Image.asset(
+                        'assets/images/icon/dark_icon.png',
+                        width: 35,
+                      ),
+                      const SizedBox(width: 16),
+                      Text(
+                        'Mode Gelap',
+                        style: GoogleFonts.rubik(
+                            fontSize: 17, fontWeight: FontWeight.w400),
+                      ),
+                      const Spacer(),
+                      Obx(() => Switch(
+                            activeColor: brownColor,
+                            activeTrackColor: lightBrownColor,
+                            inactiveThumbColor: brownColor,
+                            inactiveTrackColor: lightGrayColor,
+                            value: _themeController.isDarkMode.value,
+                            onChanged: (value) {
+                              setState(() {
+                                _themeController.changeTheme();
+                              });
+                            },
+                          )),
+                    ]),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: _colorController.getContainerColor(),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 8.0, horizontal: 16.0),
+                    child: Row(children: [
+                      Image.asset(
+                        'assets/images/icon/term_icon.png',
+                        width: 35,
+                      ),
+                      SizedBox(width: 16),
+                      Text(
+                        'Ketentuan Penggunaan',
+                        style: GoogleFonts.rubik(
+                            fontSize: 17, fontWeight: FontWeight.w400),
+                      ),
+                    ]),
+                  ),
+                ),
+                SizedBox(height: 8),
+                GestureDetector(
+                  onTap: () {
+                    AuthController.instance.logout();
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: _colorController.getContainerColor(),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8.0, horizontal: 16.0),
+                      child: Row(children: [
+                        Image.asset(
+                          'assets/images/icon/out_icon.png',
+                          width: 35,
+                        ),
+                        SizedBox(width: 16),
+                        Text(
+                          'Keluar',
+                          style: GoogleFonts.rubik(
+                              fontSize: 17, fontWeight: FontWeight.w400),
+                        ),
+                      ]),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
 import cloud_firestore
 import firebase_auth
 import firebase_core
@@ -13,6 +14,7 @@ import flutter_local_notifications
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
